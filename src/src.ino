9289ce78517e280ac3ed29c51150de7b9805aa3b
Bernhard-Hofmann <BHofmann@hotmail.co.uk>
2021-07-08 20:07:07 +0100

    Initial version displays on LCD and blinks on-board LED

---
+++ src/src.ino
@@ -0,0 +1,62 @@
+// Sourdough Monitor
+// See: https://github.com/bernhard-hofmann/SourdoughMonitor
+
+// Display
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Time of flight distance sensor
+#include <Adafruit_VL6180X.h>
+
+// DHT sensor
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+// Display
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  display.clearDisplay();
+  display.display();
+  delay(100);
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  // Not all the characters will fit on the display. This is normal.
+  // Library will draw what it can and the rest will be clipped.
+  for (int16_t i = 0; i < 256; i++) {
+    if (i == '\n') display.write(' ');
+    else          display.write(i);
+  }
+
+  display.display();
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
